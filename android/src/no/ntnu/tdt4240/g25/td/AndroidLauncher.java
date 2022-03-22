d36e360860f1edea87dac62b4628bc1f72ab6643
Bjørn Are Odden <bjoern.odden@gmail.com>
2022-03-22 11:10:07 +0100

    add generated project with Ashley

---
+++ android/src/no/ntnu/tdt4240/g25/td/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.ntnu.tdt4240.g25.td;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.ntnu.tdt4240.g25.td.TdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TdGame(), config);
+	}
+}
