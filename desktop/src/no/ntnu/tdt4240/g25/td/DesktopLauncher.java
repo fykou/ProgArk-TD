2e012f3fae6f12d5f6eb4d19e3df4ab2397b1596
Bjørn Are Odden <bjoern.odden@gmail.com>
2022-03-22 11:10:07 +0100

    add generated project with Ashley

---
+++ desktop/src/no/ntnu/tdt4240/g25/td/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package no.ntnu.tdt4240.g25.td;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import no.ntnu.tdt4240.g25.td.TdGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new TdGame(), config);
+	}
+}
