3c608cedfe99c32d956ad5d6ea2ef405a15e8928
hoyby <alex.hoeyby@gmail.com>
2022-04-22 15:09:37 +0200

    basic map classes

--- core/src/no/ntnu/tdt4240/g25/td/model/MapGrid.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/MapGrid.java
@@ -1,2 +1,39 @@
-package no.ntnu.tdt4240.g25.td.model;public class MapGrid {
+package no.ntnu.tdt4240.g25.td.model;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.List;
+import java.util.Set;
+
+public class MapGrid {
+
+    private List<MapTile> grid;
+    private Set<Vector2> path;
+    private int width;
+    private int height;
+
+    public MapGrid(int width, int height, Set<Vector2> path) {
+        this.width = width;
+        this.height = height;
+        this.path = path;
+        createMapGrid();
+    }
+
+    public void createMapGrid() {
+        int gridSize = 1;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (path.contains(new Vector2(i, j))) {
+                    grid.add(new MapTile(i, j, gridSize, MapTileType.ROAD));
+                }
+                else {
+                    grid.add(new MapTile(i, j, gridSize, MapTileType.PLAIN));
+                }
+            }
+        }
+    }
+
+    public List<MapTile> getGrid() {
+        return grid;
+    }
 }
