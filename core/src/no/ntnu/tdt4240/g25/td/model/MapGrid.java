70e435f1864f0a3e15ffaf5ec944e7db0e8147d8
Bjørn Are Odden <bjoern.odden@gmail.com>
2022-04-22 23:26:44 +0200

    add systems for maprendering, camera/viewportsystems and started changing world coords from pixels to tiles

--- core/src/no/ntnu/tdt4240/g25/td/model/MapGrid.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/MapGrid.java
@@ -1,13 +1,16 @@
 package no.ntnu.tdt4240.g25.td.model;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
-public class MapGrid {
+public class MapGrid implements Iterable<MapTile> {
 
-    private List<MapTile> grid;
+    private Array<MapTile> grid;
     private Set<Vector2> path;
     private int width;
     private int height;
@@ -20,6 +23,7 @@ public class MapGrid {
     }
 
     public void createMapGrid() {
+        grid = new Array<>(width * height);
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 if (path.contains(new Vector2(i, j))) {
@@ -32,7 +36,45 @@ public class MapGrid {
         }
     }
 
-    public List<MapTile> getGrid() {
+    public static MapGrid getTestGrid(int width, int height) {
+        // create a test grid, containing a path going from 0,0 to 9,16 , only being able
+        // to move up, down, left and right.
+        Set<Vector2> path = new HashSet<>(Arrays.asList(new Vector2(4, 0),
+                new Vector2(4, 1),
+                new Vector2(4, 2),
+                new Vector2(4, 3),
+                new Vector2(4, 4),
+                new Vector2(4, 5),
+                new Vector2(4, 6),
+                new Vector2(5, 6),
+                new Vector2(6, 6),
+                new Vector2(7, 6),
+                new Vector2(7, 7),
+                new Vector2(7, 8),
+                new Vector2(7, 9),
+                new Vector2(7, 10),
+                new Vector2(7, 11),
+                new Vector2(7, 12),
+                new Vector2(6, 12),
+                new Vector2(5, 12),
+                new Vector2(4, 12),
+                new Vector2(3, 12),
+                new Vector2(2, 12),
+                new Vector2(2, 13),
+                new Vector2(2, 14),
+                new Vector2(2, 15),
+                new Vector2(2, 16)
+        ));
+        return new MapGrid(width, height, path);
+
+    }
+
+    public Array<MapTile> getGrid() {
         return grid;
+    }
+
+    @Override
+    public Iterator<MapTile> iterator() {
+        return new Array.ArrayIterator<>(grid);
     }
 }
--- core/src/no/ntnu/tdt4240/g25/td/model/entity/factories/MobFactory.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/entity/factories/MobFactory.java
@@ -27,7 +27,7 @@ public class MobFactory extends EntityFactory {
         animationsMap.put(StateComponent.STATE_MOVING, new Animation<>(1 / 8f, assetService.getAtlasRegionArray(type.atlasPath, type.name())));
         world.createEntity().edit()
                 .add(new PositionComponent(x, y))
-                .add(new BoundsComponent(100, 100))
+                .add(new BoundsComponent(0.8f, 0.8f))
                 .add(new VelocityComponent())
                 .add(new RotationComponent(0))
                 .add(new StateComponent(StateComponent.STATE_IDLE))
