3c504180a7cebb2ed5f9704f7224f91096f4245b
Bjørn Are Odden <bjoern.odden@gmail.com>
2022-04-22 04:20:51 +0200

    camera fuckery + fonts

--- core/src/no/ntnu/tdt4240/g25/td/TdGame.java
+++ core/src/no/ntnu/tdt4240/g25/td/TdGame.java
@@ -3,20 +3,23 @@ package no.ntnu.tdt4240.g25.td;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import no.ntnu.tdt4240.g25.td.service.AssetService;
 
 public class TdGame extends Game {
 
 	private SpriteBatch batch;
+	private ShapeRenderer shapeRenderer;
 	private AssetService assetService;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-
+		shapeRenderer = new ShapeRenderer();
 		assetService = new AssetService();
 		assetService.loadTextures();
+		assetService.loadFonts(Gdx.graphics.getHeight());
 		while (!assetService.update()) {
 			// Wait for loading to complete
 		}
@@ -30,6 +33,10 @@ public class TdGame extends Game {
 
 	public SpriteBatch getBatch() {
 		return batch;
+	}
+
+	public ShapeRenderer getShapeRenderer() {
+		return shapeRenderer;
 	}
 
 }
