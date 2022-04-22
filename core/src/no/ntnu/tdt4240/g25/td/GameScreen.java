7205817f4a51014691431338a6a6192fe8cb23ad
haakgrov <haakgrov@stud.ntnu.no>
2022-04-22 19:39:58 +0200

    Add: Settings Screen and Improvements

--- core/src/no/ntnu/tdt4240/g25/td/GameScreen.java
+++ core/src/no/ntnu/tdt4240/g25/td/GameScreen.java
@@ -1,24 +1,64 @@
 package no.ntnu.tdt4240.g25.td;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Align;
 
 import no.ntnu.tdt4240.g25.td.model.GameWorld;
+import no.ntnu.tdt4240.g25.td.service.AssetService;
 
 /**
  * WIP
  */
 public class GameScreen extends ScreenAdapter {
 
+    // Game screen size
+    public int MENU_LOGIC_WIDTH = 720;
+    public int MENU_LOGIC_HEIGHT = 1280;
+
     private TdGame game;
     private Screen parent;
     private GameWorld gameWorld;
+    private SpriteBatch sb;
+    private ShapeRenderer sr;
+    private OrthographicCamera camera;
+
+    // Exit button
+    private Rectangle exitButton;
+    private GlyphLayout exitButtonLayout;
+
+    // Fonts
+    private BitmapFont font;
 
 
     public GameScreen(TdGame game, Screen parent) {
         this.game = game;
         this.parent = parent;
         this.gameWorld = new GameWorld(game.getAssetManager(), game.getBatch());
+        this.sb = game.getBatch();
+        this.sr = game.getShapeRenderer();
+        this.font = game.getAssetManager().assetManager.get(AssetService.Font.LARGE.path, BitmapFont.class);
+
+
+        exitButton = new Rectangle(0, 0, MENU_LOGIC_WIDTH / 13f, MENU_LOGIC_HEIGHT / 20f)
+                .setCenter(MENU_LOGIC_WIDTH - (MENU_LOGIC_WIDTH / 13f), MENU_LOGIC_HEIGHT - MENU_LOGIC_HEIGHT / 20f);
+
+        exitButtonLayout = new GlyphLayout(font, "X", font.getColor(), exitButton.width, Align.center, false);
+
+        // Camera
+        float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
+        camera = new OrthographicCamera(MENU_LOGIC_HEIGHT / aspectRatio, MENU_LOGIC_HEIGHT);
+        camera.position.set(MENU_LOGIC_WIDTH / 2f, MENU_LOGIC_HEIGHT / 2f, 0);
+
     }
 
     @Override
@@ -26,14 +66,40 @@ public class GameScreen extends ScreenAdapter {
 
     }
 
+    public void handleInput() {
+        if (Gdx.input.justTouched()) {
+
+            // Input coordinates
+            Vector3 inputCoordinates = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+
+            if (exitButton.contains(inputCoordinates.x, inputCoordinates.y)) {
+                game.setScreen(parent);
+            }
+        }
+    }
+
     @Override
     public void render(float delta) {
+        handleInput();
         super.render(delta);
         // just for testing/debugging as big deltas mess up stuff
         if (delta > 0.1f) {
             delta = 0.1f;
         }
         gameWorld.update(delta);
+
+        // GUI
+        camera.update();
+        sr.begin(ShapeRenderer.ShapeType.Line);
+        sr.setProjectionMatrix(camera.combined);
+        sr.setColor(Color.WHITE);
+        sr.rect(exitButton.x, exitButton.y, exitButton.width, exitButton.height);
+        sr.end();
+        sb.begin();
+        sb.setProjectionMatrix(camera.combined);
+        font.draw(sb, exitButtonLayout, exitButton.x, (exitButton.y + exitButton.height / 2f) + font.getCapHeight() / 2f);
+        sb.end();
     }
 
     @Override
--- core/src/no/ntnu/tdt4240/g25/td/SettingsScreen.java
+++ core/src/no/ntnu/tdt4240/g25/td/SettingsScreen.java
@@ -3,33 +3,75 @@ package no.ntnu.tdt4240.g25.td;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Align;
+
+import no.ntnu.tdt4240.g25.td.service.AssetService;
 
 public class SettingsScreen extends ScreenAdapter {
+
+    // Logic size menu
+    public int SETTINGS_LOGIC_WIDTH = 720;
+    public int SETTINGS_LOGIC_HEIGHT = 1280;
 
     private TdGame game;
     private Screen parent;
     private SpriteBatch sb;
+    private ShapeRenderer sr;
+    private OrthographicCamera camera;
 
     private Texture exitSettingsButton;
     private Texture menuBackground;
 
-    private int exitSettingsButtonWidth = 200;
-    private int exitSettingsButtonHeight = 100;
-    private int exitSettingsButton_posX = 100;
-    private int exitSettingsButton_posY = 100;
+    // OK Button
+    private Rectangle okButton;
+    private GlyphLayout okButtonLayout;
+
+    // SFX Checkbox
+    private Rectangle sfxCheckbox;
+    private GlyphLayout sfxCheckboxLayout;
+    private Boolean enableSFX = true;
+
+    // Fonts
+    private BitmapFont font;
 
     public SettingsScreen(TdGame game, Screen parent) {
         this.game = game;
         this.parent = parent;
         this.sb = game.getBatch();
+        this.sr = game.getShapeRenderer();
+        this.font = game.getAssetManager().assetManager.get(AssetService.Font.LARGE.path, BitmapFont.class);
 
-        this.exitSettingsButton = new Texture("screens/ok.png");
-        this.menuBackground = new Texture("screens/menuBackground.png");
+        // Camera
+        float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
+        camera = new OrthographicCamera(SETTINGS_LOGIC_HEIGHT / aspectRatio, SETTINGS_LOGIC_HEIGHT);
+        camera.position.set(SETTINGS_LOGIC_WIDTH / 2f, SETTINGS_LOGIC_HEIGHT / 2f, 0);
+
+
+        // OK Button
+        okButton = new Rectangle(0, 0, SETTINGS_LOGIC_WIDTH / 4f, SETTINGS_LOGIC_HEIGHT / 12f)
+                .setCenter(SETTINGS_LOGIC_WIDTH / 2f, (SETTINGS_LOGIC_HEIGHT - SETTINGS_LOGIC_HEIGHT / 2f));
+
+        okButtonLayout = new GlyphLayout(font, "OK", font.getColor(), okButton.width, Align.center, false);
+
+        // SFX Checkbox
+        sfxCheckbox = new Rectangle(0, 0, SETTINGS_LOGIC_HEIGHT / 24f, SETTINGS_LOGIC_HEIGHT / 24f)
+                .setCenter(SETTINGS_LOGIC_WIDTH / 2f, SETTINGS_LOGIC_HEIGHT - SETTINGS_LOGIC_HEIGHT / 8f);
+
+        sfxCheckboxLayout = new GlyphLayout(font, (enableSFX ? "X" : ""), font.getColor(), sfxCheckbox.width, Align.center, false);
+
+
+
+        //this.menuBackground = new Texture("screens/menuBackground.png");
     }
 
     @Override
@@ -41,13 +83,18 @@ public class SettingsScreen extends ScreenAdapter {
         if (Gdx.input.justTouched()) {
 
             // Input coordinates
-            Vector3 inputCoordinates = new Vector3(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY(), 0);
+            Vector3 inputCoordinates = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
 
-            // Draw bounds around OK/Exit settings Button
-            Rectangle exitSettingsButtonBounds = new Rectangle(exitSettingsButton_posX, exitSettingsButton_posY, exitSettingsButtonWidth, exitSettingsButtonHeight);
 
-            if (exitSettingsButtonBounds.contains(inputCoordinates.x, inputCoordinates.y)) {
+            // CLICK Ok button
+            if (okButton.contains(inputCoordinates.x, inputCoordinates.y)) {
                 game.setScreen(parent);
+            }
+
+            // CLICK SFX checkbox
+            else if (sfxCheckbox.contains(inputCoordinates.x, inputCoordinates.y)) {
+                enableSFX = !enableSFX;
+                sfxCheckboxLayout.setText(font, (enableSFX ? "X" : ""), font.getColor(), sfxCheckbox.width, Align.center, false);
             }
         }
     }
@@ -64,9 +111,19 @@ public class SettingsScreen extends ScreenAdapter {
             delta = 0.1f;
         }
 
+        camera.update();
+        sr.begin(ShapeRenderer.ShapeType.Line);
+        sr.setProjectionMatrix(camera.combined);
+        sr.setColor(Color.WHITE);
+        sr.rect(okButton.x, okButton.y, okButton.width, okButton.height);
+        sr.rect(sfxCheckbox.x, sfxCheckbox.y, sfxCheckbox.width, sfxCheckbox.height);
+        sr.end();
         sb.begin();
-        sb.draw(menuBackground, 0, 0);
-        sb.draw(exitSettingsButton, exitSettingsButton_posX, exitSettingsButton_posY, exitSettingsButtonWidth, exitSettingsButtonHeight);
+        sb.setProjectionMatrix(camera.combined);
+        font.draw(sb, okButtonLayout, okButton.x, (okButton.y + okButton.height / 2f) + font.getCapHeight() / 2f);
+        font.draw(sb, sfxCheckboxLayout, sfxCheckbox.x, (sfxCheckbox.y + sfxCheckbox.height / 2f) + font.getCapHeight() / 2f);
+
+        font.draw(sb, "Sound FX", sfxCheckbox.x - 200, sfxCheckbox.y + 40);
         sb.end();
     }
 
