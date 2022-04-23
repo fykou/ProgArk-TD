3644b7a917de18fb282d3eca430a0f5be2657e68
Bjørn Are Odden <bjoern.odden@gmail.com>
2022-04-23 13:37:21 +0200

    touchup on firebase classes

--- desktop/src/no/ntnu/tdt4240/g25/td/DesktopLauncher.java
+++ desktop/src/no/ntnu/tdt4240/g25/td/DesktopLauncher.java
@@ -3,6 +3,8 @@ package no.ntnu.tdt4240.g25.td;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import no.ntnu.tdt4240.g25.td.firebase.DesktopFirebaseDb;
+
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
@@ -10,6 +12,6 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setTitle("Tower Defense");
 		config.setWindowedMode(480, 854); // anything 16:9
-		new Lwjgl3Application(new TdGame(new DesktopInterfaceClass()), config);
+		new Lwjgl3Application(new TdGame(new DesktopFirebaseDb()), config);
 	}
 }
