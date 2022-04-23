04a2ef40421739ee63177c7ad46c9862be13f92f
Bjørn Are Odden <bjoern.odden@gmail.com>
2022-04-23 21:23:37 +0200

    prepping merge, trying another approach on pathing

--- core/src/no/ntnu/tdt4240/g25/td/model/entity/components/PathComponent.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/entity/components/PathComponent.java
@@ -4,26 +4,6 @@ import com.artemis.Component;
 import com.badlogic.gdx.math.Vector2;
 
 public class PathComponent extends Component {
-
-    boolean bLeft, bRight, bUp = true, bDown;
-    float previousCoordinate;
-    int checkpointNumber = 0;
-    float currentCheckpoint = 300;
-
-    public int getCheckpointNumber() {return checkpointNumber;}
-    public float getCurrentCheckpoint() {return currentCheckpoint;}
-    public float getPreviousCoordinate() {return previousCoordinate;}
-    public boolean getLeft() {return bLeft;}
-    public boolean getRight() {return bRight;}
-    public boolean getUp() {return bUp;}
-    public boolean getDown() {return bDown;}
-
-    public void setCheckpointNumber(int number) {checkpointNumber = number;}
-    public void setCurrentCheckpoint(float checkpoint) {currentCheckpoint = checkpoint;}
-    public void setPreviousCoordinate(float coordinate) {previousCoordinate = coordinate;}
-    public void setLeft(boolean left) {bLeft = left;}
-    public void setRight(boolean right) {bRight = right;}
-    public void setUp(boolean up) {bUp = up;}
-    public void setDown(boolean down) {bDown = down;}
+    public int currentCheckpoint;
 }
 
