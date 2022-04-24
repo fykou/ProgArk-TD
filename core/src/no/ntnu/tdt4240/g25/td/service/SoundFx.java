443d90f16cbf9682ca9949492e098130685f5aa3
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2022-04-24 22:44:39 +0200

    Add explosion sound when mob die

--- core/src/no/ntnu/tdt4240/g25/td/service/SoundFx.java
+++ core/src/no/ntnu/tdt4240/g25/td/service/SoundFx.java
@@ -4,10 +4,10 @@ public enum SoundFx {
     TOUCH("sounds/BUTTON_03.ogg"),
     SAVESETTINGS("sounds/UI Message Appear 01.ogg"),
     HIGHSCORE_CONFIRMED("sounds/DSGNStngr_Kill Confirm Metallic_02.ogg"),
+    EXPLODE("sounds/EXPLDsgn_Explosion Impact_14.ogg"),
     FIRE_TYPE_1("sounds/fireSounds/LASRGun_Classic Blaster A Fire_03.ogg"),
     FIRE_TYPE_2("sounds/fireSounds/LASRGun_Plasma Rifle Fire_03.ogg");
 //        BUILD("sounds/build.wav"),
-//        EXPLODE("sounds/explode.wav"),
 //        HIT("sounds/hit.wav"),
 //        MOVE("sounds/move.wav"),
 //        SELECT("sounds/select.wav"),
