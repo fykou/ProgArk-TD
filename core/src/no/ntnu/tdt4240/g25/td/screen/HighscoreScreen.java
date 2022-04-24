427cda3f5d6c3f019575dc7c9723a40e3d043e56
vemund <x16759109+Vemhas@users.noreply.github.com>
2022-04-24 12:20:55 +0200

    Add HighscoreScreen sound FX

--- core/src/no/ntnu/tdt4240/g25/td/screen/HighscoreScreen.java
+++ core/src/no/ntnu/tdt4240/g25/td/screen/HighscoreScreen.java
@@ -15,14 +15,10 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Align;
 
-import org.w3c.dom.css.Rect;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 import no.ntnu.tdt4240.g25.td.TdGame;
-import no.ntnu.tdt4240.g25.td.model.GameWorld;
 import no.ntnu.tdt4240.g25.td.service.AssetService;
 
 public class HighscoreScreen extends ScreenAdapter {
@@ -31,33 +27,34 @@ public class HighscoreScreen extends ScreenAdapter {
     public int MENU_LOGIC_WIDTH = 720;
     public int MENU_LOGIC_HEIGHT = 1280;
 
-    private TdGame game;
-    private Screen parent;
-    private SpriteBatch sb;
-    private ShapeRenderer sr;
-    private OrthographicCamera camera;
+    private final TdGame game;
+    private final Screen parent;
+    private final SpriteBatch sb;
+    private final ShapeRenderer sr;
+    private final OrthographicCamera camera;
 
     // Back to Menu Button
-    private Rectangle goBackButton;
-    private GlyphLayout goBackButtonLayout;
+    private final Rectangle goBackButton;
+    private final GlyphLayout goBackButtonLayout;
 
     // Fonts
-    private BitmapFont font;
+    private final BitmapFont font;
 
     // Top five highscores
-    private ArrayList<Map<String, String>> highscores;
+    private final ArrayList<Map<String, String>> highscores;
 
     // Sound FX
-    private Sound sound;
-    private Sound highscoreLoadSuccess;
+    private final Sound sound;
+    private final Sound highscoreLoadSuccess;
 
     public HighscoreScreen(TdGame game, Screen parent) {
-
         this.game = game;
         this.parent = parent;
         this.sb = game.getBatch();
         this.sr = game.getShapeRenderer();
         this.font = game.getAssetManager().assetManager.get(AssetService.Font.LARGE.path, BitmapFont.class);
+        this.highscoreLoadSuccess = game.getAssetManager().assetManager.get(AssetService.Sound.HIGHSCORE_CONFIRMED.path);
+        this.sound = game.getAssetManager().assetManager.get(AssetService.Sound.TOUCH.path);
 
         // Camera
         float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
@@ -70,11 +67,10 @@ public class HighscoreScreen extends ScreenAdapter {
 
         goBackButtonLayout = new GlyphLayout(font, "Back to Menu", font.getColor(), goBackButton.width, Align.center, false);
 
-
         highscores = new ArrayList<>();
 
         game.getDb().getTopFiveHighScores((ArrayList<Map<String, String>> topFiveHighScoresList) -> {
-            // Play high score load sound
+            // Play sound when highscores successfully loaded!
             long id = highscoreLoadSuccess.play(1.0f);
             highscoreLoadSuccess.setVolume(id,0.85f);
             highscoreLoadSuccess.setPitch(id, 1);
@@ -84,58 +80,26 @@ public class HighscoreScreen extends ScreenAdapter {
             highscores.addAll(topFiveHighScoresList);
             System.out.println("updated highscores");
         });
-
-        //Setting sound from game object
-
-    }
-
-
-    public void addMockupData() {
-        Map<String, String> highscore1 = new HashMap<>();
-        highscore1.put("highScore", "100");
-        highscore1.put("name", "groven");
-        highscores.add(highscore1);
-
-        Map<String, String> highscore2 = new HashMap<>();
-        highscore2.put("highScore", "50");
-        highscore2.put("name", "vemund");
-        highscores.add(highscore2);
-
-        Map<String, String> highscore3 = new HashMap<>();
-        highscore3.put("highScore", "47");
-        highscore3.put("name", "Bjørn");
-        highscores.add(highscore3);
-
-        Map<String, String> highscore4 = new HashMap<>();
-        highscore4.put("highScore", "35");
-        highscore4.put("name", "Alex");
-        highscores.add(highscore4);
-
-        Map<String, String> highscore5 = new HashMap<>();
-        highscore5.put("highScore", "14");
-        highscore5.put("name", "Crolas og Lavik");
-        highscores.add(highscore5);
     }
 
     public void handleInput() {
         if (Gdx.input.justTouched()) {
-            long id = sound.play(1.0f);
-            sound.setVolume(id,0.5f);
-            sound.setPitch(id, 1);
-            sound.setLooping(id,false);
             // Input coordinates
             Vector3 inputCoordinates = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
 
+            // Go back to parent screen and play sound
             if (goBackButton.contains(inputCoordinates.x, inputCoordinates.y)) {
+                long id = sound.play(1.0f);
+                sound.setVolume(id,0.5f);
+                sound.setPitch(id, 1);
+                sound.setLooping(id,false);
                 game.setScreen(parent);
             }
         }
     }
 
     @Override
-    public void show() {
-
-    }
+    public void show() { }
 
     @Override
     public void render(float delta) {
@@ -148,42 +112,38 @@ public class HighscoreScreen extends ScreenAdapter {
             delta = 0.1f;
         }
 
+        // Prepare the highscore screen visuals
         camera.update();
         sr.begin(ShapeRenderer.ShapeType.Line);
         sr.setProjectionMatrix(camera.combined);
         sr.setColor(Color.WHITE);
         sr.rect(goBackButton.x, goBackButton.y, goBackButton.width, goBackButton.height);
-
         sr.end();
+
         sb.begin();
         sb.setProjectionMatrix(camera.combined);
         font.draw(sb, goBackButtonLayout, goBackButton.x, (goBackButton.y + goBackButton.height / 2f) + font.getCapHeight() / 2f);
-
-
         font.draw(sb, "LEADERBOARD", MENU_LOGIC_WIDTH / 4f + 40, MENU_LOGIC_HEIGHT - 200);
         font.draw(sb, "Name", MENU_LOGIC_WIDTH / 5f, MENU_LOGIC_HEIGHT - 360);
-
         font.draw(sb, "Score", MENU_LOGIC_WIDTH / 5f + 330, MENU_LOGIC_HEIGHT - 360);
 
+        // Loop through database payload and populate top 5 highscores to screen
         for (int i = 0; i < highscores.size(); i++) {
             font.draw(sb, (i + 1) + ".", MENU_LOGIC_WIDTH / 5f, MENU_LOGIC_HEIGHT - 450 - i*100);
             font.draw(sb, highscores.get(i).get("name"), MENU_LOGIC_WIDTH / 5f + 60, MENU_LOGIC_HEIGHT - 450 - i*100);
             font.draw(sb, highscores.get(i).get("highScore"), MENU_LOGIC_WIDTH / 5f + 60 + 350, MENU_LOGIC_HEIGHT - 450 - i*100);
         }
-
         sb.end();
     }
 
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
-
     }
 
     @Override
     public void pause() {
         super.pause();
-
     }
 
     @Override
@@ -194,15 +154,10 @@ public class HighscoreScreen extends ScreenAdapter {
     @Override
     public void hide() {
         super.hide();
-
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        sound.dispose();
-        highscoreLoadSuccess.dispose();
     }
-
-
 }
