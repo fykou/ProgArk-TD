a1f993bf66c521b9c9ab01268508879a7541e05d
vemund <x16759109+Vemhas@users.noreply.github.com>
2022-04-24 22:28:28 +0200

    Change audio format to .ogg to comply with desktop env.

--- core/src/no/ntnu/tdt4240/g25/td/service/SoundFx.java
+++ core/src/no/ntnu/tdt4240/g25/td/service/SoundFx.java
@@ -1,13 +1,11 @@
 package no.ntnu.tdt4240.g25.td.service;
 
 public enum SoundFx {
-    TOUCH("sounds/BUTTON_03.wav"),
-    SAVESETTINGS("sounds/UI Message Appear 01.wav"),
-    HIGHSCORE_FLOURISH("sounds/Activate Glyph Forcefield.wav"),
-    HIGHSCORE_CONFIRMED("sounds/DSGNStngr_Kill Confirm Metallic_02.wav"),
-    GAMESTART("sounds/UI Message Appear 01.wav"),
-    FIRE_TYPE_1("sounds/fireSounds/LASRGun_Classic Blaster A Fire_03.wav"),
-    FIRE_TYPE_2("sounds/fireSounds/LASRGun_Plasma Rifle Fire_03.wav");
+    TOUCH("sounds/BUTTON_03.ogg"),
+    SAVESETTINGS("sounds/UI Message Appear 01.ogg"),
+    HIGHSCORE_CONFIRMED("sounds/DSGNStngr_Kill Confirm Metallic_02.ogg"),
+    FIRE_TYPE_1("sounds/fireSounds/LASRGun_Classic Blaster A Fire_03.ogg"),
+    FIRE_TYPE_2("sounds/fireSounds/LASRGun_Plasma Rifle Fire_03.ogg");
 //        BUILD("sounds/build.wav"),
 //        EXPLODE("sounds/explode.wav"),
 //        HIT("sounds/hit.wav"),
