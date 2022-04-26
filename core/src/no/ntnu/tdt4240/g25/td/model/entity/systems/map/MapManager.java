921f64c729fff5c79bfcad039225198926da3abd
Bjørn Are Odden <51021383+baodden@users.noreply.github.com>
2022-04-26 09:05:41 +0200

    tiledmap fuckery

--- core/src/no/ntnu/tdt4240/g25/td/model/entity/systems/map/MapManager.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/entity/systems/map/MapManager.java
@@ -2,6 +2,8 @@ package no.ntnu.tdt4240.g25.td.model.entity.systems.map;
 
 import com.artemis.BaseSystem;
 import com.artemis.annotations.Wire;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 
 import no.ntnu.tdt4240.g25.td.model.entity.components.singleton.MapComponent;
@@ -11,17 +13,9 @@ public class MapManager extends BaseSystem {
 
     private MapComponent mapComponent;
 
-    @Wire
-    MapGrid mapGrid;
-
-    @Override
     protected void initialize() {
-        mapComponent.mapGrid = mapGrid.getGrid();
-        mapComponent.path.addAll(mapGrid.getPath());
-        // convert from world coordinates to tile coordinates
-        Vector2 offset = new Vector2(.5f, .5f);
-        mapComponent.path.forEach(p -> p.add(offset));
-        setEnabled(false);
+        TiledMap map = new TmxMapLoader().load("levels/level1.tmx");
+        MapGrid mapGrid = new MapGrid(map);
     }
 
     @Override
