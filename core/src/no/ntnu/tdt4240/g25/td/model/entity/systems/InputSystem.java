9211095cdc3c426e4c72072de17d25abda2a5b6f
Bjørn Are Odden <51021383+baodden@users.noreply.github.com>
2022-04-27 12:20:40 +0200

    click checking whether tile can be built on or upgraded

--- core/src/no/ntnu/tdt4240/g25/td/model/entity/systems/InputSystem.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/entity/systems/InputSystem.java
@@ -5,8 +5,9 @@ import com.artemis.BaseSystem;
 import com.artemis.ComponentMapper;
 import com.artemis.EntitySubscription;
 import com.artemis.annotations.Wire;
+import com.artemis.utils.IntBag;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 import no.ntnu.tdt4240.g25.td.model.entity.components.PositionComponent;
@@ -14,6 +15,7 @@ import no.ntnu.tdt4240.g25.td.model.entity.components.TowerComponent;
 import no.ntnu.tdt4240.g25.td.model.entity.components.singleton.MapComponent;
 import no.ntnu.tdt4240.g25.td.model.entity.components.singleton.PlayerComponent;
 import no.ntnu.tdt4240.g25.td.model.entity.factories.TowerFactory;
+import no.ntnu.tdt4240.g25.td.model.map.MapTileType;
 import no.ntnu.tdt4240.g25.td.view.GameView;
 
 public class InputSystem extends BaseSystem implements InputProcessor {
@@ -44,8 +46,25 @@ public class InputSystem extends BaseSystem implements InputProcessor {
 
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         Vector3 click = cameraSystem.viewport.unproject(new Vector3(screenX, screenY, 0));
-        float tileX = (int) click.x;
-        float tileY = (int) click.y;
+        int tileX = (int) click.x;
+        int tileY = (int) click.y;
+        // check if the tile is empty/can be built on
+        MapTileType tileType = map.getTileTypeByCoordinate(0, tileX, tileY);
+        if (tileType == MapTileType.PLAIN) {
+            // check if the spot already has a tower
+            IntBag towerIds = towerSubscription.getEntities();
+            for (int i = 0; i < towerIds.size(); i++) {
+                int towerId = towerIds.get(i);
+                PositionComponent towerPos = mPosition.get(towerId);
+                if ((int)towerPos.get().x == tileX && (int)towerPos.get().y == tileY) {
+                    Gdx.app.log("InputSystem", "Tower already exists");
+                    // upgrade trigger
+                }
+                else {
+                    // buy trigger
+                }
+            }
+        }
         gameView.triggerBuyDialogue(tileX, tileY, player.cash);
         return true;
     }
