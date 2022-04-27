295a626293533022c71e1cec6b1f76b0d3e92a18
Bjørn Are Odden <51021383+baodden@users.noreply.github.com>
2022-04-27 12:20:40 +0200

    click checking whether tile can be built on or upgraded

--- core/src/no/ntnu/tdt4240/g25/td/model/map/MapTileType.java
+++ core/src/no/ntnu/tdt4240/g25/td/model/map/MapTileType.java
@@ -2,15 +2,22 @@ package no.ntnu.tdt4240.g25.td.model.map;
 
 public enum MapTileType {
 
-    PLAIN("plain"),
-    OBSTACLE("obstacle"),
-    TREES("trees"),
-    ROAD("road"),
-    ROAD_TURN("road_turn");
+    PLAIN(0, "plain"),
+    OBSTACLE(1, "obstacle"),
+    TREES(2, "trees"),
+    ROAD(3, "road"),
+    ROAD_TURN(4, "road_turn");
 
     public final String regionName;
+    public final int id;
 
-    MapTileType(String regionName) {
+    MapTileType(int id, String regionName) {
+        this.id = id;
         this.regionName = regionName;
+    }
+    public static MapTileType getTileTypeById(int id) {
+        // the id modulo 7 is used to get the tile type
+        // from the id.
+        return MapTileType.values()[(id - 1) % 7];
     }
 }
--- core/src/no/ntnu/tdt4240/g25/td/model/map/Point.java
+++
@@ -1,7 +0,0 @@
-package no.ntnu.tdt4240.g25.td.model.map;
-
-import com.badlogic.gdx.maps.MapObject;
-
-public class Point extends MapObject {
-
-}
