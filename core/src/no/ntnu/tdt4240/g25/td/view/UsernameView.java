efea0c3ca3495dd356db06880fecd9a1a0917fb2
Bjørn Are Odden <51021383+baodden@users.noreply.github.com>
2022-04-27 18:46:33 +0200

    fixing merge, checking for empty

--- core/src/no/ntnu/tdt4240/g25/td/view/UsernameView.java
+++ core/src/no/ntnu/tdt4240/g25/td/view/UsernameView.java
@@ -13,19 +13,20 @@ import com.badlogic.gdx.utils.Align;
 import no.ntnu.tdt4240.g25.td.asset.Assets;
 import no.ntnu.tdt4240.g25.td.asset.Audio;
 import no.ntnu.tdt4240.g25.td.asset.SoundFx;
-import no.ntnu.tdt4240.g25.td.controller.UsernameScreen;
+import no.ntnu.tdt4240.g25.td.controller.UsernameController;
 
 public class UsernameView extends AbstractView {
 
     private final Skin skin = Assets.getInstance().getSkin();
     private final TextButton playButton = new TextButton("Play", skin);
+    private final Label errorLabel = new Label("", skin);
     private final TextField userInput;
-    private final Table table = new Table();
+    private final Table table = new Table(skin);
     private String playerName;
 
-    private final UsernameScreen.ViewCallbackHandler viewCallback;
+    private final UsernameController.ViewCallbackHandler viewCallback;
 
-    public UsernameView(SpriteBatch batch, UsernameScreen.ViewCallbackHandler viewCallback) {
+    public UsernameView(SpriteBatch batch, UsernameController.ViewCallbackHandler viewCallback) {
         super(viewport, batch);
         Gdx.input.setInputProcessor(this);
         this.viewCallback = viewCallback;
@@ -34,13 +35,17 @@ public class UsernameView extends AbstractView {
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+                if (userInput.getText().length() <= 0) {
+                    showError("Please enter a username");
+                    return;
+                }
                 playerName = userInput.getText();
                 setPlayerName();
             }
         });
     }
 
-    public void setPlayerName(){
+    public void setPlayerName() {
         Audio.playFx(SoundFx.TOUCH);
         viewCallback.setPlayerNameOnFirebaseObject(playerName);
         viewCallback.toMenu();
@@ -51,16 +56,25 @@ public class UsernameView extends AbstractView {
         Gdx.input.setInputProcessor(this);
         // NEW table
         Label title = new Label("Enter username", skin, "default");
-        title.setFontScale(3);
+
         table.setFillParent(true);
         table.add(title).padBottom(50).colspan(2).row();
+        table.add(errorLabel).padBottom(50).colspan(2).align(Align.center).row();
         table.add(userInput).align(Align.center).size(400, 75).padBottom(25).colspan(2).row();
         table.add(playButton).size(350, 90).colspan(2).row();
 
+        userInput.setTextFieldListener((textField, c) -> errorLabel.setVisible(false));
+
         // Adjust sizes
         playButton.getLabel().setFontScale(2);
-
+        title.setFontScale(3);
+        errorLabel.setFontScale(2);
         getRoot().addActor(table);
+    }
+
+    public void showError(String error) {
+        errorLabel.setText(error);
+        errorLabel.setVisible(true);
     }
 
     @Override
@@ -71,7 +85,6 @@ public class UsernameView extends AbstractView {
 
     @Override
     public void pause() {
-
     }
 
     @Override
